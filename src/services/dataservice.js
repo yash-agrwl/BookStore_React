51a0bd92dc8f947ccf90f62a91642b228e9b3475
yash-agrwl <agarwalyash7012@gmail.com>
2022-08-18 17:20:14 +0530

    refactored dataservice.js in order to update the headerConfig whenever any api gets called

--- src/services/dataservice.js
+++ src/services/dataservice.js
@@ -1,38 +1,57 @@
 import axios from "axios"
 
-const headerConfig = {
-    headers: {
-        Authorization: "Bearer " + localStorage.getItem('token')
+// const headerConfig = {
+//     headers: {
+//         Authorization: "Bearer " + localStorage.getItem('token')
+//     }
+// }
+
+const setToken = () => {
+
+    const token = localStorage.getItem('token');
+
+    const headerConfig = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
     }
+
+    return headerConfig;
 }
 
 export const getAllBooks = () => {
     // console.log(headerConfig)
+    const headerConfig = setToken();
     let response = axios.get("https://localhost:44356/api/Book/GetAll", headerConfig)
     return response
 }
 
 export const getCartItems = () => {
+    const headerConfig = setToken();
     let response = axios.get("https://localhost:44356/api/Cart/GetAllCartItems", headerConfig)
     return response
 }
 
 export const updateCart = (bookId, bookCount) => {
+    const headerConfig = setToken();
     let response = axios.patch(`https://localhost:44356/api/Cart/UpdateCart?bookId=${bookId}&count=${bookCount}`, null, headerConfig)
     return response
 }
 
 export const addToCart = (bookId) => {
+    const headerConfig = setToken();
     let response = axios.post(`https://localhost:44356/api/Cart/AddToCart?bookId=${bookId}`, null, headerConfig)
     return response
 }
 
 export const addFeedback = (addFeedbackObj) => {
+    const headerConfig = setToken();
     let response = axios.post("https://localhost:44356/api/Feedback/Add", addFeedbackObj, headerConfig)
     return response
 }
 
 export const getAllFeedback = (bookId) => {
+    const headerConfig = setToken();
     let response = axios.get(`https://localhost:44356/api/Feedback/GetAll?bookId=${bookId}`, headerConfig)
     return response
 }
